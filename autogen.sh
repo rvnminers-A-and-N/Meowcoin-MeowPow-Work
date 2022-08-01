bbdc3e815b7bf9ae5a13ff7f124c0ee2af022608
AMStarks <78468155+AMStarks@users.noreply.github.com>
2022-08-01 14:53:50 +1000

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Copyright (c) 2013-2016 The Bitcoin Core developers
-# Copyright (c) 2017-2019 The Raven Core developers
+# Copyright (c) 2017-2022 The Raven Core developers
+# Copyright (c) 2022-Eternity The Meow Core developers, team and community
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
